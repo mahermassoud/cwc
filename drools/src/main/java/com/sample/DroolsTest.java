a379fe384039c0f3db619338f22672c06186ca8c
massoudTheDude <mahermassoud@gmail.com>
2015-10-21 11:11:44 -0700

    Got rule working, added diagnostic output, new gene sets and input files

--- drools/src/main/java/com/sample/DroolsTest.java
+++ drools/src/main/java/com/sample/DroolsTest.java
@@ -14,35 +14,28 @@ public class DroolsTest {
 	
 
     public static final void main(String[] args) {
-        /*
+
     	try {
             // load up the knowledge base
 	        KieServices ks = KieServices.Factory.get();
     	    KieContainer kContainer = ks.getKieClasspathContainer();
         	KieSession kSession = kContainer.newKieSession("ksession-rules");
 
-            // go !
-            Message message = new Message();
-            message.setMessage("Hello World");
-            message.setStatus(Message.HELLO);
-            
-            Message boomMessage = new Message();
-            boomMessage.setMessage("boom?");
-            
-            kSession.insert(message);
-            kSession.insert(boomMessage);
-            
+            // Read in input list and check for relevance
+        	GeneListInputAgent test_input = new GeneListInputAgent("epidermal_test.txt");
+        	kSession.insert(test_input);
+        	
             kSession.fireAllRules();
         } 
         catch (Throwable t) {
             t.printStackTrace();
         }
-        */
         
         GeneListInputAgent input = new GeneListInputAgent("gene_list.txt");
         System.out.print(input.getArray().toString());
     }
-    
+   
+    /*
     public static class Message {
 
         public static final int HELLO = 0;
@@ -69,5 +62,6 @@ public class DroolsTest {
         }
 
     }
+    */
 
 }
--- drools/src/main/java/com/sample/GeneListInputAgent.java
+++ drools/src/main/java/com/sample/GeneListInputAgent.java
@@ -14,17 +14,34 @@ import java.io.FileNotFoundException;
  * This class is for providing input to the DroolsTest class. 
  * It is a placeholder for interfacing with the natural language group.
  * 
+ * Since this is for testing, gene constants are not realistic, reflect relevance to this pathway
+ * http://cgap.nci.nih.gov/Genes/PathGeneQuery?PAGE=1&ORG=Hs&PATH=h_At1rPathway
+ * 
  * @author Massoud Maher
  * @version 0.1
  */
 public class GeneListInputAgent {
 	
+	// Reads in file
 	private BufferedReader reader = null;
+	
 	// Array of each line in file
 	private ArrayList<String> fileLines;
 	
+	// Name of input file
+	private String fileName;
+	
+	// Genes involved in epidermal growth factor receptor
 	public static final List<String> GENE_EPIDERMAL_GROWTH_FACTOR_RECEPTOR = 
 			Arrays.asList("NM_005228", "NM_201284", "NM_201282", "NM_201283");
+
+	// Genes involved in myocyte enhancer 2c 
+	public static final List<String> GENE_MYOCYTE_ENHANCER_2C =
+			Arrays.asList("NM_002397", "NM_001193350", "NM_001193347", "NM_001131005", "NM_001193348", "NM_001193349");
+	
+	// Genes involved in calmodulin 1
+	public static final List<String> GENE_CALMODULIN_1 =
+			Arrays.asList("NM_006888");
 	
 	/**
 	 * Creates an GeneListInputAgent for reading in a text file.
@@ -32,6 +49,7 @@ public class GeneListInputAgent {
 	 * @param fileName Filename of file wanted to be read in
 	 */
 	public GeneListInputAgent(String fileName) {
+		this.fileName = fileName;
 		try {
 			File filePath = new File( this.getClass().getResource(fileName).toURI());
 			System.err.println(filePath.toString() + ": FILEPATH");
@@ -91,6 +109,15 @@ public class GeneListInputAgent {
 	 */
 	public ArrayList<String> getArray() {
 		return fileLines;
+	}
+	
+	/**
+	 * Returns name of input file
+	 * 
+	 * @return String that is name of input file
+	 */
+	public String getFileName() {
+		return fileName;
 	}
 }
 
