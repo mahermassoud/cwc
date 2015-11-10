71a7bf41231246ca027e0e35ed1c83d0ac4aa3ab
massoudTheDude <mahermassoud@gmail.com>
2015-11-10 13:25:50 -0800

    Reverted to resolve input issue

--- drools/src/main/java/com/sample/DroolsTest.java
+++ drools/src/main/java/com/sample/DroolsTest.java
@@ -36,37 +36,6 @@ public class DroolsTest {
             t.printStackTrace();
         }
         
-        //GeneListInputAgent input = new GeneListInputAgent("gene_list.txt");
-        //System.out.print(input.getArray().toString());
     }
    
-    /*
-    public static class Message {
-
-        public static final int HELLO = 0;
-        public static final int GOODBYE = 1;
-
-        private String message;
-
-        private int status;
-
-        public String getMessage() {
-            return this.message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-
-        public int getStatus() {
-            return this.status;
-        }
-
-        public void setStatus(int status) {
-            this.status = status;
-        }
-
-    }
-    */
-
 }
